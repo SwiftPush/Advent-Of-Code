971601d802df1421a2facc8f08c2f1c2a6840f31
Andy Provan <AndyProvan@gmail.com>
2017-12-24 19:54:16 +0000

    Day 4, Part 1 and 2. WIP

--- 04-high-entropy-passphrases/src/main.rs
+++ 04-high-entropy-passphrases/src/main.rs
@@ -1,14 +1,47 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::io::{self};
 
 #[test]
-fn examples() {
-    assert_eq!(is_valid("aa bb cc dd ee"), true);
-    assert_eq!(is_valid("aa bb cc dd aa"), false);
-    assert_eq!(is_valid("aa bb cc dd aaa"), true);
+fn examples_part1() {
+    assert_eq!(is_valid_line("aa bb cc dd ee"), true);
+    assert_eq!(is_valid_line("aa bb cc dd aa"), false);
+    assert_eq!(is_valid_line("aa bb cc dd aaa"), true);
 }
 
-fn is_valid(input: &str) -> bool {
+#[test]
+fn is_anagram_tests() {
+    assert_eq!(is_anagram("", ""), true);
+    assert_eq!(is_anagram("anna", "nana"), true);
+    assert_eq!(is_anagram("bob", "obb"), true);
+    assert_eq!(is_anagram("qwerty", "asdfg"), false);
+    assert_eq!(is_anagram("aa", "a"), false);
+    assert_eq!(is_anagram("abb", "aab"), false);
+}
+
+fn is_anagram(s1: &str, s2: &str) -> bool {
+    if s1.len() != s2.len() { return false; }
+
+    let mut map = HashMap::new();
+
+    for c in s1.chars() {
+        let value = match map.get(&c) {
+            Some(value) => value + 1,
+            None => 1
+        };
+        map.insert(c, value);
+    }
+
+    for c in s2.chars() {
+        if map.contains_key(&c) == false { return false; }
+        let value = map.get(&c).unwrap() - 1;
+        if value < 0 { return false; }
+        map.insert(c, value);
+    }
+
+    true
+}
+
+fn is_valid_line(input: &str) -> bool {
     let mut words = HashSet::new();
     let iter = input.split_whitespace();
     for word in iter {
@@ -29,7 +62,7 @@ fn main() {
         io::stdin().read_line(&mut line).unwrap();
         if line.is_empty() { break; }
 
-        if is_valid(&line) { number_of_valid_lines += 1; }
+        if is_valid_line(&line) { number_of_valid_lines += 1; }
     }
 
     println!("number_of_valid_lines={}", number_of_valid_lines);
